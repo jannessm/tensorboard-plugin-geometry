21ddde95592914811ce10a6f553b218bbc3803ed
Jannes Magnusson <j-magnusson@t-online.de>
2020-09-10 09:41:32 +0200

    add plot div and resize

--- tensorboard_plugin_geometry/static/src/app/data-card/data-card.ts
+++ tensorboard_plugin_geometry/static/src/app/data-card/data-card.ts
@@ -22,11 +22,20 @@ interface Tag {
 export default class DataCardComponent extends Vue {
   tag_regex = '';
   data = {
-    expanded: true
+    expanded: true,
+    plot_size: 100
   }
 
   constructor() {
     super();
-    this.$nextTick(() => (<MdCard>this.$children[0]).MdCard.expand = this.data.expanded);
+    this.$nextTick(() => {
+      (<MdCard>this.$children[0]).MdCard.expand = this.data.expanded;
+      this.data.plot_size = Math.floor(Math.max( 1 / this.$props.tag.length, 0.33) * 100);
+      console.log(this.data.plot_size);
+    });
+  }
+
+  max(a: number, b: number) {
+    return a > b ? a : b;
   }
 }
--- tensorboard_plugin_geometry/static/src/app/plot/plot.ts
+++ tensorboard_plugin_geometry/static/src/app/plot/plot.ts
@@ -9,8 +9,25 @@ import SliderComponent from '../slider/slider';
 
 import {Scene} from 'three';
 
-interface Tag {
-  [run: string]: number // run: #(samples)
+enum CONTENT_TYPES {
+  UNDEFINED = 'undefined',
+  VERTICES = 'vertices',
+  FACES = 'faces',  // Triangle face.
+  FEATURES = 'features'
+}
+
+interface Steps {
+  [step: number]: {
+    wall_time: number;
+    config: object;
+    vertices: Metadata;
+    features: Metadata;
+    faces: Metadata;
+  }
+}
+
+interface Metadata {
+  shape: number[];
 }
 
 @WithRender
@@ -23,39 +40,79 @@ interface Tag {
 export default class PlotComponent extends Vue {
   tag_regex = '';
   data = {
-    step: 0,
-    total_steps: 0,
-    wall_time: new Date(),
-    wall_times: [],
+    current_step: 0,
+    current_wall_time: new Date(),
+    steps: {},
+    max_step: 0,
+    plot_height: '20vw',
+    fullscreen: false,
   };
+
+  default_class = '';
 
   constructor() {
     super();
     this.getData();
+    this.$nextTick().then(() => {
+      this.data.plot_height = (this.$children[0].$parent.$el as HTMLElement).offsetWidth + 'px';
+    });
   }
 
   async getData() {
     await this.$nextTick;
     const res = await ApiService.getMetadata(this.$props.run.name, this.$props.tag);
-
-    const vertices = res.data.filter(val => val.content_type === 1);
-    this.data.total_steps = vertices.reduce((max, val) => max ? val.step < max : val.step , -9999999);
-    this.data.wall_times = vertices.map(val => val.wall_time);
     
-    this.updateStep(this.data.total_steps);
+    res.data.forEach(element => {
+      this._addStep(element.step, element.content_type, element.shape, element.wall_time, element.config);
+    });
+    
+    this.data.max_step = Object.values(this.data.steps).length - 1;
+
+    this.updateStep(this.data.max_step);
   }
 
   updateStep(new_value: number) {
     // update header
-    this.data.step = new_value;
-    this.data.wall_time = new Date(this.data.wall_times[new_value] * 1000);
+    this.data.current_step = new_value;
+    this.data.current_wall_time = new Date(this.data.steps[new_value].wall_time * 1000);
 
     // update plot
-    this.updatePlot(new_value, this.data.wall_times[new_value]);
+    this.updatePlot(new_value, this.data.steps[new_value].wall_time);
   }
 
   async updatePlot(step: number, wall_time: number) {
     const res = await ApiService.getData(this.$props.run.name, this.$props.tag, step, wall_time);
     console.log(res);
+  }
+
+  togglePlotSize() {
+    this.data.fullscreen = !this.data.fullscreen;
+
+    const this_html_el = this.$parent.$children[0].$el;
+
+    if (!this.default_class) {
+      this.default_class = (this_html_el.className.match(/md-size-\d+/) as string[])[0];
+    }
+
+    if (this.data.fullscreen) {
+      this_html_el.classList.replace(this.default_class, 'md-size-100');
+    } else {
+      this_html_el.classList.replace('md-size-100', this.default_class);
+    }
+  
+    this.data.plot_height = (this.$children[0].$parent.$el as HTMLElement).offsetWidth + 'px';
+  }
+
+  _addStep(step: number, content_type: CONTENT_TYPES, shape: number[], wall_time: number, config: string) {
+    if (!this.data.steps[step]) {
+      this.data.steps[step] = {
+        wall_time,
+        config: JSON.parse(config)
+      };
+    }
+
+    this.data.steps[step][content_type] = {
+      shape
+    };
   }
 }
